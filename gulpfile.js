cea4c6fdad9afd05d6610a58ef7eb0967b8b498d
John Preston <wcerfgba@riseup.net>
2016-07-22 13:33:25 +0100

    Install and configure Webpack, Babel.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
+var webpack = require('webpack-stream');
 
-gulp.task('default', [ 'bower' ]);
+gulp.task('default', [ 'bower', 'react' ]);
 
 gulp.task('bower', function () {
   gulp.src('bower_components/**')
     .pipe(gulp.dest('web/assets/vendor/'));
+});
+
+gulp.task('react', function () {
+  gulp.src('react/index.js')
+    .pipe(webpack( require('./webpack.config.js') ))
+    .pipe(gulp.dest('web/assets/'));
 });
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+}
